52cc245127a5bcd93fa23c44f0be29158c2afe71
Igor Martins Pinheiro <igordf@gmail.com>
2021-04-17 00:35:21 -0300

    test: ensure SignUpController return 400 if no password provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,19 @@ describe('SignUp Controller', () => {
         expect(httpReponse.statusCode).toBe(400)
         expect(httpReponse.body).toEqual(new MissingParamError('email'))
     });
+
+    test('Should return 400 if no password provided ', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                passwordConfirmation: 'any_password'
+
+            }
+        }
+        const httpReponse = sut.handle(httpRequest)
+        expect(httpReponse.statusCode).toBe(400)
+        expect(httpReponse.body).toEqual(new MissingParamError('password'))
+    });
 });
